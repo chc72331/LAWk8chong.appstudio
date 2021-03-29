9493497deaf8e72407c6c3960c22cbe94f270655
chc72331 <chc72331@creighton.edu>
2021-03-29 15:16:42 -0500

    Update favExercises.js

--- forms/favExercises/favExercises.js
+++ forms/favExercises/favExercises.js
@@ -1,10 +1,18 @@
 var exercises = ["pullups","pushups","situps","jogging","plank","curls"]
 var coreExercises = ["situps","plank"]
 
-favExercises.onshow=function(){
-
+favExercises.onshow = function(){
+  for (i = 0; i < exercises.length; i ++)
+    selExercises.addItem(exercises[i])
 }
 
-
-
-
+let message = ''
+btnExercise.onclick=function(){
+  for (j = 0; j < selExercises.text.length; j++)
+    message = message + selExercises.text[j]
+    
+  if (message == "situps, plank, ")
+    console.log("You right")
+  else
+    console.log("Not core exercises")
+}
