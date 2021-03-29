df86e94e1b61e74a5373f409060accabce758840
chc72331 <chc72331@creighton.edu>
2021-03-29 15:16:33 -0500

    Update mobileNav.js

--- forms/mobileNav/mobileNav.js
+++ forms/mobileNav/mobileNav.js
@@ -2,6 +2,11 @@
 hmbMenu.onclick=function(s){
   if (typeof(s) == "object") 
     return
+  switch(s) {
+  case "Dessert Voting"
+    changeForm(dessertVoting)
+    break
+  case
   /* add a switch here to take the user to the form
      they chose. 
   */
