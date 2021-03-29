62a09b202e0d9a76e9443e7977f4b968fb25148c
chc72331 <chc72331@creighton.edu>
2021-03-29 15:16:47 -0500

    Update dessertVoting.js

--- forms/dessertVoting/dessertVoting.js
+++ forms/dessertVoting/dessertVoting.js
@@ -3,6 +3,8 @@ var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
 
 dessertVoting.onshow=function(){
     drpDesserts.clear()   
+    for (i = 0; i < dessertList.length; i ++)
+      drpDesserts.addItem(dessertList[i])
   /* add a loop here that adds all the desserts in the array 
      to the dropdown.
   */
@@ -15,8 +17,12 @@ drpDesserts.onclick=function(){
     if (typeof(s) == "object")   
       return                    
     else {  // the user picked something
+        drpDesserts.value = s
+        console.log(s)
+        
         /* add code here to get item user chose and output 
         it in a label, using a literal.
         */
     }
 }
+
