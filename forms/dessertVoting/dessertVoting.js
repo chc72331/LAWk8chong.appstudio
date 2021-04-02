cd2f4403d6a27ff5a34f604f3e16d21f88ab8024
chc72331 <chc72331@creighton.edu>
2021-04-02 11:30:40 -0500

    Update dessertVoting.js

--- forms/dessertVoting/dessertVoting.js
+++ forms/dessertVoting/dessertVoting.js
@@ -1,7 +1,7 @@
 var dessertList = ['Chocolate Cake','Cream Brulee','Cheesecake',
                   'Keyline Pie','Cherry Cobbler','Chocolate Chip Cookies']
 
-dessertVoting.onshow=function(){
+dessertVoting.onshow = function() {
     
     // First, let's clear whatever shows up on the dropdown
     drpDesserts.clear()
@@ -9,15 +9,17 @@ dessertVoting.onshow=function(){
       drpDesserts.addItem(dessertList[i])
 }
 
-
-drpDesserts.onclick=function(){
+drpDesserts.onclick = function(s) {
 // this 'if' kicks user out if they  just clicked on control 
 // but not on one item in the list.
     if (typeof(s) == "object")   
       return                    
     else {  // the user picked something
         drpDesserts.value = s
-        console.log(s)
-    s}
+        lblDessertMessage.value = `${s}?  Okay, if you insist.`
+    }
 }
 
+btnDescribeYou.onclick = function(){
+  ChangeForm(describeYou)
+}
