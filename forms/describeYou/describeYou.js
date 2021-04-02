8d0f6faddf04486cbaed5b051b138d50c640783b
chc72331 <chc72331@creighton.edu>
2021-04-02 11:41:29 -0500

    Update describeYou.js

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,8 +1,9 @@
-rdbtnBlah.onchange = function() {
-  let userMood = $(rdbtnBlah.value)
-  console.log(userMood)
+rdbtnTraits.onchange = function() {
+  let userMood = $("input[name = rdbtnTraits]:checked").prop("value")
+  userMood = userMood.toLowerCase()
+  lblDescribeMessage.value = `I would 
+          also say that you\'re ${userMood} too!`
 }
-
 
 btnFavEx.onclick = function(){
   ChangeForm(favExercises)
